c67a593c931e2ee142479f27eb2e149d8e77e3de
Alexis Benoist <alexis.benoist@gmail.com>
2013-12-23 11:14:37 +0100

    Commencé le modèle de la base de données mais il y a encore des petits problèmes.

---
+++ ws/invoicing/app.py
@@ -0,0 +1,17 @@
+#!flask/bin/python
+# -*- coding: utf-8 -*-
+import os
+from flask import Flask, abort
+from flask.ext.sqlalchemy import SQLAlchemy
+from flask.ext.restful import Api
+
+config_obj = os.environ.get("DIAG_CONFIG_MODULE", "config_dev")
+
+app = Flask(__name__)
+app.config.from_object(config_obj)
+
+api = Api(app)
+db = SQLAlchemy(app)
+
+if __name__ == '__main__':
+    app.run(debug=True)
