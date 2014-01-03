88d97a556cd7222f7285030adb5ccb17ffb32d7d
Alexis Benoist <alexis.benoist@gmail.com>
2014-01-03 23:21:19 +0100

    Invoicing: Commencé à implémenter balance mais ca marche pas.

--- ws/invoicing/balanceAPI.py
+++ ws/invoicing/balanceAPI.py
@@ -1,7 +1,8 @@
 #!flask/bin/python
 # -*- coding: utf-8 -*-
 from flask_restful import Resource, marshal
-from model import Printing, LoadingCreditCard, HelpDesk
+from sqlalchemy.sql import func, exists
+from model import Printing, LoadingCreditCard, HelpDesk, Transaction
 from fields import printing_fields, loading_credit_card_fields, help_desk_fields
 from app import db
 
@@ -15,9 +16,17 @@ class BalanceAPI(Resource):
         GET the balance of a given user.
         """
         #TODO check if the User ID exists in the user DB.
+        #If there is no transaction the balance is 0.
 
-
-
-
-
-
+        # if not db.session.query(exists().where(Transaction.id==id)).scalar():
+        #     return {
+        #         'user_id': id,
+        #         'balance': 0
+        #     }
+        print(db.session.query(Transaction).all())
+        balance = db.session.query(func.sum(Transaction.amount).label('sum'))\
+                    .filter(Transaction.id == id).scalar()
+        return {
+                'user_id':id,
+                'balance': balance
+            }
