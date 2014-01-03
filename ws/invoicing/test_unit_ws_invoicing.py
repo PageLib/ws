4bae1b717b6516b704471afbc1e0b92d40c39292
Alexis Benoist <alexis.benoist@gmail.com>
2014-01-03 13:25:44 +0100

    WS Invoicing: Started assertTransactionEquals

--- ws/invoicing/test_unit_ws_invoicing.py
+++ ws/invoicing/test_unit_ws_invoicing.py
@@ -22,9 +22,18 @@ class InvoiceTestCase(unittest.TestCase):
     def assertJsonContentType(self, rv):
         self.assertEquals(rv.headers['Content-type'], 'application/json')
 
+    def assertTransactionEquals(self, transaction, user, amount, currency, transaction_type, date_time=None):
+        self.assertEquals(transaction['user'], user)
+        self.assertEquals(transaction['amount'], amount)
+        #TODO transform amount to numbers.
+        self.assertEquals(transaction['currency'], currency)
+        self.assertEquals(transaction['transaction_type'], transaction_type)
+        if date_time is not None:
+            self.assertEquals(transaction['date_time'], date_time)
+
     def test_invoice_list_post_loading_credit_card(self):
         """
-        POST a transaction then get it.
+        POST a transaction and checks the answer.
         """
         # POST
         ref_transaction = {
@@ -33,9 +42,12 @@ class InvoiceTestCase(unittest.TestCase):
             'currency': 'EUR',
             'transaction_type': 'loading_credit_card'
         }
-        rv_post = self.app.post('/v1/invoices', data=json.dumps(ref_transaction), content_type='application/json')
+        rv_post = self.app.post('/v1/invoices', data=json.dumps(ref_transaction),
+                                content_type='application/json')
         self.assertJsonContentType(rv_post)
         self.assertEquals(rv_post.status_code, 201)
-        print(rv_post.data)
-
-        #GET
+        resp = json.loads(rv_post.data)
+        self.assertTransactionEquals(resp, ref_transaction['user'],
+                                     ref_transaction['amount'],
+                                     ref_transaction['currency'],
+                                     ref_transaction['transaction_type'])
