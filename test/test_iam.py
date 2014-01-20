5df1522001416c700e99c346456cfcdd2ef63e65
Raphael Kolm <raphael.kolm@gmail.com>
2014-01-20 22:29:06 +0100

    IAM test_edit_user

--- test/test_iam.py
+++ test/test_iam.py
@@ -39,7 +39,38 @@ class IamTestCase(WsTestCase):
 
     def test_edit_user(self):
         """Create and modify a user."""
-        pass
+        rv_post = requests.post(self.iam_endpoint + '/v1/users',
+                                data=json.dumps(self.ref_user),
+                                headers={'Content-type': 'application/json'})
+        user_id = rv_post.json()['id']
+
+        put_data = {
+            'login': 'jack.foo',
+            'last_name': 'Foo',
+            'first_name': 'Jack',
+            'role': 'manager'
+        }
+
+        # Edit the user
+        rv_put = requests.put(self.iam_endpoint + '/v1/users/' + user_id,
+                              data=json.dumps(put_data),
+                              headers={'Content-type': 'application/json'})
+        self.assertJsonAndStatus(rv_put, 200)
+        json_put = rv_put.json()
+
+        # Get it
+        rv_get = requests.get(self.iam_endpoint + '/v1/users/' + user_id)
+        self.assertJsonAndStatus(rv_get, 200)
+        json_get = rv_get.json()
+
+        # Assert the concerned fields are modified in both the PUT and GET responses
+        for field in ('login', 'last_name', 'first_name', 'role'):
+            self.assertIn(field, json_put, 'missing field ' + field + ' in PUT response')
+            self.assertIn(field, json_get, 'missing field ' + field + ' in GET response')
+            self.assertEquals(json_put[field], put_data[field],
+                              'mismatch on field ' + field + ' in PUT response')
+            self.assertEquals(json_get[field], put_data[field],
+                              'mismatch on field ' + field + ' in GET response')
 
     def test_create_user_twice(self):
         """Try to create the same user twice, assert failure on the second trial."""
