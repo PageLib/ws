1e3c503502890499641bdf3fc21397ad01fd4bbe
Raphael Kolm <raphael.kolm@gmail.com>
2014-01-22 14:53:30 +0100

    Refactoring MyAPI (format des réponses)

--- ws/common/MyAPI.py
+++ ws/common/MyAPI.py
@@ -1,16 +1,12 @@
 from flask_restful import Api
-from flask_restful.utils import error_data
 
 
 class MyApi(Api):
-    """
-    This error handler transform every flask-restful 400 error in a 412.
-    """
     def handle_error(self, e):
-        code = getattr(e, 'code', 500)
-        data = getattr(e, 'data', error_data(code))
-        if code == 400:
-            r = self.make_response({'error': data}, 412)
+        """Transform a Flask-RESTful parsing error (HTTP 400) in a 412 with the inner error
+            message."""
+        if e.code == 400 and hasattr(e, 'data') and ('message' in e.data):
+            r = self.make_response({'error': e.data['message']}, 412)
             r.headers['Content-type'] = 'application/json'
             return r
         return Api.handle_error(self, e)
