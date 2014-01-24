f911858ce0279e9763694686c992528e4d8c78ce
Alexis Benoist <alexis.benoist@gmail.com>
2014-01-24 13:37:08 +0100

    IAM:UserListAPI comments + GET

--- ws/iam/UserListAPI.py
+++ ws/iam/UserListAPI.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from flask_restful import Resource, reqparse, marshal
 import model
+from model import User
 from flask import request
 from fields import user_fields
 from ws.common.helpers import generate_uuid_for
@@ -14,9 +15,10 @@ class UserListAPI(Resource):
 
     def post(self):
         """Creates a new user"""
+        # Parse the arguments.
         parser = reqparse.RequestParser()
-        parser.add_argument('login', type=str, location='json')
-        parser.add_argument('password', type=str, location='json')
+        parser.add_argument('login', type=str, location='json', required=True)
+        parser.add_argument('password', type=str, location='json', required=True)
         parser.add_argument('role', type=str, location='json')
         parser.add_argument('last_name', type=str, location='json')
         parser.add_argument('first_name', type=str, location='json')
@@ -25,24 +27,23 @@ class UserListAPI(Resource):
         first_name = args.get('first_name', None)
         last_name = args.get('last_name', None)
         role = args.get('role', None)
+        login = args['login']
+        password = args['password']
+        
+        #Check if the role is correct
         if role not in roles or role is None:
             app.logger.warning('Request on POST UserListAPI for non existing or missing role')
             return {'error': 'Role POSTed is not allowed'}, 412
 
-        try:
-            login = args['login']
-            password = args['password']
-        except KeyError:
-            app.logger.warning('Request on POST UserListAPI with missing login or password in json')
-            return {}, 412
-
         # We check if another non deleted user has the same login
-        user_same_login_exists = request.dbs.query(exists().where(and_(model.User.login == login,
-                                                                   not_(model.User.deleted)))).scalar()
+        user_same_login_exists = request.dbs.query(exists().where(and_(User.login == login,
+                                                                   not_(User.deleted)))).scalar()
         if user_same_login_exists:
             return {'error': 'User with the same login exists.'}, 412
-        user_id = generate_uuid_for(request.dbs, model.User)
-        u = model.User(
+        
+        # Write the user in DB.
+        user_id = generate_uuid_for(request.dbs, User)
+        u = User(
             id=user_id,
             login=login,
             password_hash=hashlib.sha1(password).hexdigest(),
@@ -56,6 +57,25 @@ class UserListAPI(Resource):
         return marshal(u.to_dict(), user_fields), 201
 
     def get(self):
-        pass
+        parser = reqparse.RequestParser()
+        parser.add_argument('login', type=str, location='values')
+        parser.add_argument('first_name', type=str, location='values')
+        parser.add_argument('last_name', type=str, location='values')
+        args = parser.parse_args()
+        
+        query = request.dbs.query(User)
+        
+        # Optional filters
+        if args.get('login', None):
+            query = query.filter(User.login == args['login'])
+
+        if args.get('first_name', None):
+            query = query.filter(User.first_name == args['first_name'])
+
+        if args.get('last_name', None):
+            query = query.filter(User.login == args['last_name'])
+
+        return {'users': map(lambda u: marshal(u.to_dict(), user_fields, query.all()))}
+
 
 from app import app
