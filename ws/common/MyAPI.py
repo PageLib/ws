eaf3268edd9b07517d31c55cc7e5bf3b8360936e
Alexis Benoist <alexis.benoist@gmail.com>
2014-02-05 16:21:45 +0100

    Fix bug in MyAPI if e has no code attribute

--- ws/common/MyAPI.py
+++ ws/common/MyAPI.py
@@ -5,8 +5,9 @@ class MyApi(Api):
     def handle_error(self, e):
         """Transform a Flask-RESTful parsing error (HTTP 400) in a 412 with the inner error
             message."""
-        if e.code == 400 and hasattr(e, 'data') and ('message' in e.data):
-            r = self.make_response({'error': e.data['message']}, 412)
-            r.headers['Content-type'] = 'application/json'
-            return r
+        if hasattr(e, 'code') and hasattr(e, 'data'):
+            if e.code == 400 and ('message' in e.data):
+                r = self.make_response({'error': e.data['message']}, 412)
+                r.headers['Content-type'] = 'application/json'
+                return r
         return Api.handle_error(self, e)
